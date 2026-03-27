248757916d8e8bee056124d82903fd42379a794e
Naveen Vijayan <navinbs0614@gmail.com>
2026-03-27 20:18:27 +0530

    new changes

--- app/utils/adminApi.ts
+++ app/utils/adminApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const adminAPI = axios.create({
-  baseURL: "http://localhost:5000/api", // 🔥 change if deployed
+  baseURL: "https://diet-chart-9wl9.onrender.com/api", // 🔥 change if deployed
 });
 
 adminAPI.interceptors.request.use((config) => {
